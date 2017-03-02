c0cb1f79879d342b4913d3d10b79629abed8c4de
Chadi Kazan <kazan.chadi@gmail.com>
2017-03-02 22:57:14 +0100

    proper react-native-vector-icons linking

--- android/app/src/main/java/com/bookmarker/MainApplication.java
+++ android/app/src/main/java/com/bookmarker/MainApplication.java
@@ -3,6 +3,8 @@ package com.bookmarker;
 import android.app.Application;
 import android.util.Log;
 
+import com.oblador.vectoricons.VectorIconsPackage;
+
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -24,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new VectorIconsPackage()
       );
     }
   };
